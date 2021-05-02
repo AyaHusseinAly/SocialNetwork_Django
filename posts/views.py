ae635692cd95b048468acca2da6f6629641a6819
aya hussein <aya.hussein3107@gmail.com>
2021-05-02 02:30:54 +0200

    create new post

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from .models import Post
 from groups.models import Group
+from .forms import PostForm
+
 #from django.contrib.auth.decorators import login_required, permission_required
 
 
@@ -10,9 +12,15 @@ from groups.models import Group
 def index(request):
     posts= Post.objects.all()
     groups= Group.objects.all()
-
+    post= PostForm(request.POST or None)
+    if post.is_valid():
+        form_content=post.cleaned_data['content']
+        post_obj=Post.objects.create( content=form_content,owner=request.user)
+        post_obj.save()
+        return redirect("index")
     return render(request,"posts/index.html",{
         "posts":posts,
         "groups":groups
 
     })
+
