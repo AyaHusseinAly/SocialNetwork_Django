dcdaa465b8519466f3510cef3859a0908d8c1532
MariamDawa <mariamsamirdawa@gmail.com>
2021-05-02 02:07:47 +0200

    search functions added

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from .models import Post
-from groups.models import Group
+from django.contrib.auth.models import User
+# from groups.models import Group
 #from django.contrib.auth.decorators import login_required, permission_required
 
 
@@ -9,10 +10,23 @@ from groups.models import Group
 #@permission_required(["books.view_book"],raise_exception=True)
 def index(request):
     posts= Post.objects.all()
-    groups= Group.objects.all()
-
-    return render(request,"posts/index.html",{
+    query=request.GET.get('q','')
+    if(query):
+        first_name_query1=User.objects.filter(first_name__contains=str(query))
+        first_name_query2=User.objects.filter(first_name__in=[query])
+        last_name_query1=User.objects.filter(last_name__contains=str(query))
+        last_name_query2=User.objects.filter(last_name__in=[query])
+        users = first_name_query1.union(first_name_query1,last_name_query1,last_name_query2)
+        return render(request,"users/index.html",{
+        "users":users,
+        })
+    else:
+        posts = Post.objects.all()
+        return render(request,"posts/index.html",{
         "posts":posts,
-        "groups":groups
+        # "groups":groups
 
-    })
+        })
+    # groups= Group.objects.all()
+
+    
