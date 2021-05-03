e1d7d284f1a7246c6fbc4193714c9f5f6f47cbf7
shrook <shrooktageldin@gmail.com>
2021-05-03 02:11:07 +0200

    group page and its posts

--- socialMedia/settings.py
+++ socialMedia/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# MEDIA_URL = '/media/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+MEDIA_URL = '/image/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'posts/static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'posts/static/images')
