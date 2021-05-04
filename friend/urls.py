46e01fce51f9014c3eb496b539d023f6fa0fb8d4
MariamDawa <mariamsamirdawa@gmail.com>
2021-05-04 18:30:09 +0200

    friend request decline added

--- friend/urls.py
+++ friend/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from friend.views import (
     send_friend_request,
+    decline_friend_request,
 )
 
 app_name = "friend"
 
 urlpatterns = [
     path('friend_request/', send_friend_request, name="friend-request"),
+    path('decline_friend_request/<int:id>', decline_friend_request, name="friend-request-decline"),
+
 ]
