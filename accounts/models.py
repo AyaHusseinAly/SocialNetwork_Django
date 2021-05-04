e3d2a679cfdee1268e740e83cf0d76b219a96b77
shrook <shrooktageldin@gmail.com>
2021-05-04 15:04:18 +0200

    get memebers of specefic group, modify create group form page

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from groups.models import Group
 # class UserCreateForm(UserCreationForm):
 #     avatar = forms.CharField(required=True)
 #     email= forms.CharField(required=True)
@@ -17,18 +18,17 @@ from django.contrib.auth.forms import UserCreationForm
 #         return user
 
 # Create your models here.
+
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    country = models.CharField(max_length=30,null=True,blank=True)
-    avatar=models.ImageField(null=True,blank=True)
-    birthdate = models.DateField(auto_now=False, auto_now_add=False,null=False,blank=False)
-    about=models.TextField(max_length=500,null=True,blank=True)
-    # groups=models.ManyToManyField(Group,blank=True,null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    country = models.CharField(max_length=30, null=True, blank=True)
+    avatar = models.ImageField(null=True, blank=True)
+    birthdate = models.DateField(
+        auto_now=False, auto_now_add=False, null=False, blank=False)
+    about = models.TextField(max_length=500, null=True, blank=True)
+    groups = models.ManyToManyField(Group, blank=True, null=True)
     # posts=models.ManyToManyField(Post,blank=True,null=True)
-
-
-
-
 
     def __str__(self):
         return self.user.username
