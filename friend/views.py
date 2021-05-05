fe8e282694683cf7e1e422711e8ce425b88ca357
MariamDawa <mariamsamirdawa@gmail.com>
2021-05-05 02:52:34 +0200

    view friend requests added and routing for profile edited

--- friend/views.py
+++ friend/views.py
@@ -2,24 +2,25 @@ from django.shortcuts import render , redirect
 from django.http import HttpResponse
 import json
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 
 
 from accounts.models import UserProfile
 from friend.models import FriendRequest,FriendList
 
+@login_required(login_url="/login")
 def friend_requests(request, *args, **kwargs):
     context = {}
     user = request.user
-    if user.is_authenticated:
-        user_id = kwargs.get("user_id")
-        account =  User.objects.get(pk=user_id)
-        if account == user:
-            friend_requests = FriendRequest.objects.filter(receiver= account, is_active= True)
-            context['friend_requests'] = friend_requests
-        else:
-            return HttpResponse("You can't view another users friend requests.")
-    #else:
-    #    redirect("login")
+
+    user_id = kwargs.get("user_id")
+    account =  User.objects.get(pk=user_id)
+    if account == user:
+        friend_requests = FriendRequest.objects.filter(receiver= account, is_active= True)
+        context['friend_requests'] = friend_requests
+    else:
+        return HttpResponse("You can't view another users friend requests.")
     return render(request, "friend/friend_requests.html", context)
 
 def send_friend_request(request, *args, **kwargs):
